3b1a244b45513ffa857f57ef9e6e036600123d5e
Veimar Choque <veimar.choque@truextend.com>
2019-10-22 15:58:31 -0400

    Footer-modified: Footer changed with the new features and design

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,23 +3,73 @@
     dark
     padless
   >
+
+
     <v-card
       class="flex"
       flat
       tile
     >
-      <v-card-title class="teal">
-        <strong class="subheading">¡Escribenos a nuestras redes sociales!</strong>
-        <v-spacer></v-spacer>
-        <v-btn
-          v-for="icon in icons"
-          :key="icon"
-          class="mx-4"
-          dark
-          icon
-        >
-          <v-icon size="24px">{{ icon }}</v-icon>
-        </v-btn>
+      <v-card-title class="pink darken-1" >
+        <v-container class="pink darken-1" >
+          <v-row no-gutters >
+            <v-col>
+              <v-card
+                class="transparent"
+                outlined
+                tile
+              >
+                <v-layout justify-center>
+                 <strong>¡Escribenos a nuestras redes sociales!</strong>
+                  <v-spacer></v-spacer>
+                </v-layout>
+              </v-card>
+              <v-card
+                class="transparent"
+                outlined
+                tile
+              >
+                <v-layout justify-center>
+                  <v-btn
+                    v-for="icon in icons"
+                    :key="icon"
+                    class="mx-4"
+                    dark
+                    icon
+                  >
+                    <v-icon size="24px">{{ icon }}</v-icon>
+                  </v-btn>
+                </v-layout>
+             </v-card>  
+            </v-col>
+
+            <v-col>
+              <v-card 
+                class="transparent"
+                outlined
+                tile
+              >
+                <v-layout justify-center>
+                  <strong >WIT</strong>
+                </v-layout>
+              </v-card>
+            </v-col>
+            <v-col>
+              <v-card
+                class="transparent"
+                outlined
+                tile
+              >
+               <v-layout justify-center>
+                Third
+                 </v-layout>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+
+
+        
       </v-card-title>
 
       <v-card-text class="py-2 white--text text-center">
