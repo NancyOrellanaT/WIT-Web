438e807c5504b66e4432baa3d41fd77843558d1b
Adriana <adrianaorellana34@gmail.com>
2019-10-23 19:24:58 -0400

    Cover-main: carousel and parallax added

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,25 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+    <Carousel />
+    <HelloWorld />
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+
+import Carousel from './components/Carousel'
+// import CoverMain from './components/CoverMain'
+import HelloWorld from './components/HelloWorld'
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld,
+    Carousel,
+    // CoverMain,
+    HelloWorld
   },
   data: () => ({
     //
-  }),
+  })
 };
 </script>
