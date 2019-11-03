65955874a3f9304dc35857c2dea4a72cb1f91d22
heidy choque ledezma <kete_heidy@hotmail.com>
2019-11-03 00:40:56 -0400

    metrics1:Responsive problems solved

--- src/components/Metricas.vue
+++ src/components/Metricas.vue
@@ -1,40 +1,21 @@
 <template>
   <v-container>
    <h2 align="center">METRICAS</h2>
-   <v-row>
-        <v-col>
-            <h3 align="center"> {{titles.t1}} </h3>
-        </v-col>
-        <v-col>
-            <h3 align="center"> {{titles.t2}} </h3>
-        </v-col>
-        <v-col>
-            <h3 align="center"> {{titles.t3}} </h3>
-        </v-col>
-   </v-row>
-   <v-row justify="space-around" >
-    
-       <v-img v-for="image in images" :src="image.src" :key="image" max-height="300" max-width="300" aspect-ratio="1"> </v-img>
-    
-   </v-row>
-   <v-row justify="space-around">
-        <v-col>
-         <v-layout justify-center>
-            <blockquote class="blockquote">{{descriptions.d1}}</blockquote>
-        </v-layout>
-       
-        </v-col>
-        <v-col>
-         <v-layout justify-center>
-            <blockquote class="blockquote">{{descriptions.d2}}</blockquote>
-        </v-layout>
-        </v-col>
-        <v-col>
-         <v-layout justify-center>
-            <blockquote class="blockquote">{{descriptions.d3}}</blockquote>
-        </v-layout>
-        </v-col>
-   </v-row>
+   <v-row >
+   <v-col  v-for="component in components" :key="component" cols="12" sm="4" md="4">
+       <v-card  >
+        <v-row align="center" justify="center" style="height: 50px;" >
+        <h3 align="center" justify="center"> {{component.t}} </h3>
+        </v-row>
+        <v-img :src="component.src" :aspect-ratio="1.3">
+        </v-img>
+        <div align="center" justify="center">
+       <blockquote class="blockquote" >{{component.d}}</blockquote>
+        </div>
+       </v-card>
+   </v-col>
+</v-row>
+
   </v-container>
 </template>
 
@@ -42,22 +23,12 @@
 export default{
     data: () => (
         {
-            titles: {
-                t1: 'Diversidad',
-                t2: 'Comunidades',
-                t3: 'Mujeres trabajando'
-            },
-            images: [
-                {src: require('@/assets/diversidad.jpg')},
-                {src: require('@/assets/comunidad.jpg')},
-                {src: require('@/assets/poder.jpg')}
-            ],
-            descriptions: {
-                d1: 'Las miembros de las comunidades, son mujeres con experiencia en diferentes ramas de la tecnologia',
-                d2: 'Existen 8 comunidades en Cochabamba que trabajan por la integracion de la mujer en el area de la tecnologia ',
-                d3: 'Se ha roto esa brecha de genero, mostrando las capacidades de la mujer'
+            components: [
+                {t: 'Diversidad', src: require('@/assets/diversidad.jpg'), d: 'Las miembros de las comunidades, son mujeres con experiencia en diferentes ramas de la tecnologia'},
+                {t: 'Comunidades', src: require('@/assets/comunidad.jpg'), d: 'Existen 8 comunidades en Cochabamba que trabajan por la integracion de la mujer en el area de la tecnologia '},
+                {t: 'Mujeres trabajando', src: require('@/assets/poder.jpg'),d: 'Se ha roto esa brecha de genero, mostrando las capacidades de la mujer'}
+            ]
 
-            }
         }
     )
 }
